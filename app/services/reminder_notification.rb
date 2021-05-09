65448a0545de60e08906cfb8c99cc5238b7ad785
Abhishek Kanungo <nemisyz@gmail.com>
2021-05-10 04:30:31 +0530

    reminder delayed email changes

--- app/services/reminder_notification.rb
+++ app/services/reminder_notification.rb
@@ -18,14 +18,14 @@ class ReminderNotification
     
     def send_one_hour_reminder_mail
         if @task.incomplete? 
-            ReminderMailer.one_hour_reminder(@task) 
+            ReminderMailer.one_hour_reminder(@task).deliver_now
             @reminder.one_hour_reminder_sent
         end    
     end    
 
     def send_twenty_four_hour_reminder_mail
         if @task.incomplete?
-            ReminderMailer.twenty_four_hour_reminder(@task)
+            ReminderMailer.twenty_four_hour_reminder(@task).deliver_now
             @reminder.twenty_four_hour_reminder_sent
         end  
     end      
